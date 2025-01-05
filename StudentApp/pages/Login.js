a53e5ebcddf42a857fecb98473a1d6ef23a38559
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-01-05 17:37:53 +0530

    Update Loging page

--- StudentApp/pages/Login.js
+++ StudentApp/pages/Login.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { StyleSheet, View, Image } from 'react-native';
 import Header from '../components/Header'; 
-
+import { TextInput ,Button} from 'react-native-paper';
+import {students} from '../StudentDb'
 const Login = () => {
+  
   return (
     <View style={styles.container}>
       <Header />
@@ -11,6 +13,23 @@ const Login = () => {
           source={require('../assets/logo.png')} 
           style={styles.logo}
         />
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          mode="outlined"
+          label="Username"
+          placeholder="Enter your Username here"
+          style={styles.textinput}
+        />
+        <TextInput
+          label="Password"
+          secureTextEntry
+          right={<TextInput.Icon icon="eye" />}
+          style={styles.textinput}
+        />
+        <Button  mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>
+          Login
+        </Button>
       </View>
     </View>
   );
@@ -33,6 +52,19 @@ const styles = StyleSheet.create({
     height: '100%', 
     resizeMode: 'contain', 
   },
+  form:{
+    flex:1,
+    alignItems:'center',
+    width:'100%'
+  },
+  textinput:{
+    width:'80%',
+    margin:15
+  },
+  button:{
+    width:'80%',
+    margin:15
+  }
 });
 
 export default Login;
