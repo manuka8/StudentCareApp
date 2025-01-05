5c676e16500dcb340d17011cb9c9cbaacb6b3082
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-01-05 19:16:28 +0530

    Addin new fronr-tend route

--- StudentApp/App.js
+++ StudentApp/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './pages/Login'; 
-
+import Profile from './pages/Profile'
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -12,6 +12,11 @@ export default function App() {
         <Stack.Screen
           name="Login"
           component={Login}
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
           options={{ headerShown: false }} 
         />
       </Stack.Navigator>
